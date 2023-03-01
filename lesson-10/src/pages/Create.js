5890a0b24e7bd009b8ed53530b26f24be5288120
Borscs András <borscsa1@gmail.com>
2023-03-01 20:01:56 +0100

    lesson-10

--- lesson-10/src/pages/Create.js
+++ lesson-10/src/pages/Create.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import {Container, Typography} from "@mui/material";
+import Button from '@mui/material/Button';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
+import {makeStyles} from "@material-ui/core";
+
+
 
 export default function Create() {
-  return (
-    <div>
-      Create page
-    </div>
-  )
+
+    return (
+        <Container size="lg">
+            <Typography
+                variant='h6'
+                align='center'
+                color='primary'
+                component='h2'
+                gutterBottom
+            >
+                Create a new notes
+            </Typography>
+            <Button
+                onClick={() => console.log('WORKING')}
+                type="submit"
+                color="secondary"
+                variant="contained"
+                endIcon={<AccessAlarmIcon/>}
+            >
+                Submit
+            </Button>
+            <br/>
+            <AcUnitOutlinedIcon/>
+            <AcUnitOutlinedIcon color="secondary" fontSize="large"/>
+            <AcUnitOutlinedIcon color="secondary" fontSize="small"/>
+            <AcUnitOutlinedIcon color="action" fontSize="small"/>
+            <AcUnitOutlinedIcon color="error" fontSize="large"/>
+            <AcUnitOutlinedIcon color="disabled" fontSize="small"/>
+        </Container>
+    )
 }
